36d095186c4d541e30059a4867c5e169768af16c
Sergio Almecija <sheniff@gmail.com>
2016-02-03 09:09:13 -0800

    First proof of concept: check google auth and retrieve email address

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,8 +1,6 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router'
-import { actions as counterActions } from '../../redux/modules/counter'
-import DuckImage from './Duck.jpg'
+import { actions as gmailActions } from 'redux/modules/gmail'
 import classes from './HomeView.scss'
 
 // We define mapStateToProps where we'd normally use
@@ -11,45 +9,49 @@ import classes from './HomeView.scss'
 // the component can be tested w/ and w/o being connected.
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
 const mapStateToProps = (state) => ({
-  counter: state.counter
+  email: state.gmail.email,
+  loading: state.gmail.loading
 })
+
+const actions = Object.assign({}, gmailActions)
+
 export class HomeView extends React.Component {
   static propTypes = {
-    counter: PropTypes.number.isRequired,
-    doubleAsync: PropTypes.func.isRequired,
-    increment: PropTypes.func.isRequired
+    email: PropTypes.string.isRequired,
+    loading: React.PropTypes.bool.isRequired,
+    checkAuth: PropTypes.func.isRequired,
+    connect: PropTypes.func.isRequired
   };
 
+  componentDidMount () {
+    this.props.checkAuth()
+  }
+
   render () {
+    const email = this.props.email
+    const loading = this.props.loading
     return (
-      <div className='container text-center'>
-        <div className='row'>
-          <div className='col-xs-2 col-xs-offset-5'>
-            <img className={classes.duck}
-                 src={DuckImage}
-                 alt='This is a duck, because Redux.' />
-          </div>
-        </div>
-        <h1>Welcome to the React Redux Starter Kit</h1>
-        <h2>
-          Sample Counter:
-          {' '}
-          <span className={classes['counter--green']}>{this.props.counter}</span>
-        </h2>
-        <button className='btn btn-default'
-                onClick={() => this.props.increment(1)}>
-          Increment
+      <div className='container'>
+        <h1>
+          Gmail<small>ite</small>
+        </h1>
+
+        <span className={ loading ? '' : classes.hidden }>
+          Loading...
+        </span>
+
+        <button
+          onClick={this.props.connect}
+          className={ !loading && !email ? '' : classes.hidden }>
+          Connect To Gmail
         </button>
-        {' '}
-        <button className='btn btn-default'
-                onClick={this.props.doubleAsync}>
-          Double (Async)
-        </button>
-        <hr />
-        <Link to='/404'>Go to 404 Page</Link>
+
+        <h1 className={ !loading && email ? '' : classes.hidden }>
+          Welcome, {email}
+        </h1>
       </div>
     )
   }
 }
 
-export default connect(mapStateToProps, counterActions)(HomeView)
+export default connect(mapStateToProps, actions)(HomeView)
